c5ccf54fe266afce7eb55a6ff543205f0d93d9d1
Alexandre458 <143671650+Alexandre458@users.noreply.github.com>
2024-06-14 18:08:00 -0300

    Create GrepoData City Indexer-2.0.0.user.js
    Atualização

---
+++ Concluídos/GrepoData City Indexer-2.0.0.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         GrepoData Auto_Indexar
+// @namespace    grepodata
+// @version      2.0.0
+// @author       BadUser
+// @homepage     https://grepodata.com/indexer
+// @updateURL    https://api.grepodata.com/script/indexer.user.js
+// @downloadURL	 https://api.grepodata.com/script/indexer.user.js
+// @description  Esse script permite a automátização da indexação dos relatórios do jogo.
+// @include      https://*.grepolis.com/game/*
+// @include      https://grepodata.com*
+// @exclude      view-source://*
+// @icon         https://grepodata.com/assets/images/grepodata_icon.ico
+// ==/UserScript==
+
+(function() {
+    var rand = Math.floor((Date.now()/1000)/(60*60)) + "";
+    var CustomStyleJS = document.createElement('script');
+    CustomStyleJS.type = 'text/javascript';
+    CustomStyleJS.src = 'https://api.grepodata.com/script/indexer.js?v=' + rand;
+    document.getElementsByTagName("head")[0].appendChild(CustomStyleJS);
+    var CustomStyleCSS = document.createElement('link');
+    CustomStyleCSS.rel = 'stylesheet';
+    CustomStyleCSS.type = 'text/css';
+    CustomStyleCSS.href = 'https://api.grepodata.com/script/indexer.css?v=' + rand;
+    document.getElementsByTagName("head")[0].appendChild(CustomStyleCSS);
+    console.log("Added GrepoData City Indexer by Tamper/GreaseMonkey");
+})();
